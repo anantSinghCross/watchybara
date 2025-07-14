07c08deb69e480453dd1461f4c516cd9ab5c0149
Anant Singh <38243636+anantSinghCross@users.noreply.github.com>
2025-07-14 14:10:01 +0530

    Update package dependencies and enhance popup styling with Tailwind CSS

--- popup.tsx
+++ popup.tsx
@@ -1,24 +1,22 @@
 import { useState } from "react"
+import './styles.css'
 
 function IndexPopup() {
   const [data, setData] = useState("")
 
   return (
-    <div
-      style={{
-        padding: 16
-      }}>
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
+    <div className="bg-blue-400">
+      <div className="rounded-2xl p-4 bg-blue-50 text-sm border-4 border-blue-400">
+        <h2>
+          Welcome to your{" "}
+          <a href="https://www.plasmo.com" target="_blank">
+            Plasmo
+          </a>{" "}
+          Extension!
+        </h2>
+        <input className="my-2 p-2 outline-blue-400 rounded-lg" onChange={(e) => setData(e.target.value)} value={data} />
+        <p>{data}</p>
+      </div>
     </div>
   )
 }
